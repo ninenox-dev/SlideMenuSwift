4c7167cb0a1f4ecd4e7fd1a836ae328e2061c7d1
Ninenox <nisit15@hotmail.com>
2017-04-05 21:02:48 +0700

    Initial Commit

---
+++ TestSlideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSlideMenu
+//
+//  Created by Nisit Sirimarnkit on 4/5/2560 BE.
+//  Copyright © 2560 Nisit Sirimarnkit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
